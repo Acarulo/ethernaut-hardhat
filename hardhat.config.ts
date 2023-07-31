406ab554d09b2f2435abcbecc4076d44bb5ba11a
Acarulo <augusto.e.caro@gmail.com>
2023-07-31 16:48:07 -0300

    Level 19 setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.5.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        }
+      },
+      {
         version: "0.6.12",
         settings: {
           optimizer: {
